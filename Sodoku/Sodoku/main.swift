6a10f33d1f189f4a7fe1c1dfb837ab847fa3ec15
Juliang0705 <juliang0705@gmail.com>
2015-12-19 16:57:02 -0600

    Rewrite the code in object orinted fashion

--- Sodoku/Sodoku/main.swift
+++ Sodoku/Sodoku/main.swift
@@ -1,163 +1,22 @@
-//: Playground - noun: a place where people can play
 
-import Foundation
+let easy1:String = "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
+let east2:String = "600057010408106003010203080050674000970000000080920050140080039703009825002060041"
+let hard1:String = "850002400720000009004000000000107002305000900040000000000080070017000000000036040"
 
-func cross(A A:String,B:String)->[String]{
-    var result = [String]()
-    for a in A.characters{
-        for b in B.characters{
-            result.append("\(a)\(b)")
-        }
-    }
-    return result
-}
-
-var cellLabels:[String] = cross(A:"ABCDEFGHI", B: "123456789")
-var subGrids:[[String]] = [
-    ["A1","A2","A3","B1","B2","B3","C1","C2","C3"],
-    ["A4","A5","A6","B4","B5","B6","C4","C5","C6"],
-    ["A7","A8","A9","B7","B8","B9","C7","C8","C9"],
-    ["D1","D2","D3","E1","E2","E3","F1","F2","F3"],
-    ["D4","D5","D6","E4","E5","E6","F4","F5","F6"],
-    ["D7","D8","D9","E7","E8","E9","F7","F8","F9"],
-    ["G1","G2","G3","H1","H2","H3","I1","I2","I3"],
-    ["G4","G5","G6","H4","H5","H6","I4","I5","I6"],
-    ["G7","G8","G9","H7","H8","H9","I7","I8","I9"]
-]
-
-func createPeerMap(labels: [String],subGrids: [[String]]) -> [String: [String]]{
-    var result = [String: [String]]()
-    for label in labels{
-        var values:Set<String> = Set<String>()
-        
-        let firstLetter = label[label.startIndex]
-        for (var n = 1; n < 10; ++n){
-            values.insert("\(firstLetter)" + "\(n)")
-        }
-        let secondNumber = label[label.startIndex.advancedBy(1)]
-        for letter in "ABCDEFGHI".characters{
-            values.insert("\(letter)" + "\(secondNumber)")
-        }
-        outerLoop:        for subGrid in subGrids{
-            for elem in subGrid{
-                if elem == label{
-                    for v in subGrid{
-                        values.insert(v)
-                    }
-                    values.remove(label)//don't contain itself
-                    result.updateValue(Array(values), forKey: label)
-                    break outerLoop
-                }
-            }
-        }
-    }
-    return result
+var solver:SodokuSolver = SodokuSolver()
+do{
+    try solver.solve(sodokuFromString: east2)
+    try solver.printFormattedSodoku()
+    print(try solver.getRawSodoku())
+    
+}catch SodokuError.ParsingError{
+    print("Error in parsing the sodoku")
+}catch SodokuError.IncorrectSodokuError{
+    print("This sodoku has no solution")
+}catch SodokuError.UnsolvedSodokuError{
+    print("Call solve() function before print")
+}catch is ErrorType{
+    print("Unexpected Error")
 }
 
-var peerMap = createPeerMap(cellLabels, subGrids: subGrids)
 
-
-func parseSudoku(source:String) -> [String: Set<Int>]{
-    var sodoku = [String: Set<Int>]()
-    //    print (source.characters.count)
-    assert(source.characters.count == 81)
-    for i in 0..<cellLabels.count{
-        let number: Int = Int(String(source[source.startIndex.advancedBy(i)]))!
-        let label:String = cellLabels[cellLabels.startIndex.advancedBy(i)]
-        if number != 0 {
-            sodoku.updateValue(Set([number]), forKey: label)
-        }else{
-            sodoku.updateValue(Set([1,2,3,4,5,6,7,8,9]), forKey: label)
-        }
-    }
-    return sodoku
-}
-func printSodoku(let sodoku:[String: Set<Int>]){
-    var counter = 0
-    for label in cellLabels{
-        ++counter
-        for n in sodoku[label]!{
-            print(n,terminator:"")
-        }
-        print("  ",terminator:"")
-        if counter % 9 == 0{
-            print("")
-        }else if counter % 3 == 0{
-            print("|",terminator:"")
-        }
-    }
-}
-let source:String = "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
-let source2:String = "850002400720000009004000000000107002305000900040000000000080070017000000000036040"
-var mySodoku = parseSudoku(source2)
-//printSodoku(mySodoku)
-func eliminate(inout sodoku:[String: Set<Int>]) -> Bool{
-    for label in cellLabels{
-        if sodoku[label]!.count == 1{
-            let peers = peerMap[label]!
-            let value = sodoku[label]!.first!
-            if eliminatePeers(&sodoku, peers: peers,valueToRemove: value) == false{
-                return false
-            }
-        }
-    }
-    return true
-}
-func eliminatePeers(inout sodoku:[String: Set<Int>],let peers:[String],valueToRemove:Int) -> Bool{
-    for label in peers{
-        if sodoku[label]!.contains(valueToRemove) == false {
-            continue
-        }
-        if sodoku[label]!.count == 1 && sodoku[label]!.first! != valueToRemove{
-            continue
-        }
-        sodoku[label]!.remove(valueToRemove)
-        if sodoku[label]!.count == 0{
-            return false
-        }else if sodoku[label]!.count == 1{
-            let value = sodoku[label]!.first!
-            let newPeers = peerMap[label]!
-            if eliminatePeers(&sodoku, peers: newPeers, valueToRemove: value) == false{
-                return false
-            }
-        }
-    }
-    return true
-}
-func isSolved(let sodoku:[String: Set<Int>]) -> Bool{
-    for label in cellLabels{
-        if sodoku[label]!.count != 1{
-            return false
-        }
-    }
-    return true
-}
-func cellWithleastPossibilities(let sodoku:[String: Set<Int>]) -> String{
-    var current:String = cellLabels.first!
-    for label in cellLabels{
-        if sodoku[current]!.count == 1 {
-            current = label
-        }
-        else if sodoku[label]!.count != 1 && sodoku[label]!.count < sodoku[current]!.count{
-            current = label
-        }
-    }
-    return current
-}
-var result:[String:Set<Int>]?
-func solveSodoku(let sodoku:[String:Set<Int>]){
-    if isSolved(sodoku){
-        result = sodoku
-    }
-    let targetCell:String = cellWithleastPossibilities(sodoku)
-    for possibility in sodoku[targetCell]!{
-        var sodokuCopy = sodoku
-        sodokuCopy[targetCell]!.remove(possibility)
-        if (eliminate(&sodokuCopy)){
-             solveSodoku(sodokuCopy)
-        }
-    }
-}
-eliminate(&mySodoku)
-solveSodoku(mySodoku)
-printSodoku(result!)
